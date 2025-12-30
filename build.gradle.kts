c370796373b476cf8c8551d2de8d6c2a903a3cc7
lex-lopez <l3xlop@gmail.com>
2025-12-29 18:54:49 -0600

    Updating gradle and gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,13 @@ repositories {
 
 dependencies {
     implementation("io.temporal:temporal-sdk:1.31.0")
+    implementation("net.minidev:json-smart:2.5.2")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.15.0")
+    implementation("io.grpc:grpc-netty-shaded:1.75.0")
     testImplementation("io.temporal:temporal-testing:1.31.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("net.minidev:json-smart:2.5.2")
 }
 
 tasks.test {
