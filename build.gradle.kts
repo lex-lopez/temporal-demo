69ad6ed1da987649c38411ed5c11dd4fed255197
lex-lopez <l3xlop@gmail.com>
2026-01-02 20:52:23 -0600

    Updating build.gradle to patch transitive vulnerabilities

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,16 +10,33 @@ repositories {
 }
 
 dependencies {
-    implementation("io.temporal:temporal-sdk:1.31.0")
+    implementation("io.temporal:temporal-sdk:1.32.1")
+    implementation("org.slf4j:slf4j-nop:2.0.17")
+    testImplementation("io.temporal:temporal-testing:1.31.0")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:5.20.0")
+
+    // The below are needed to patch a vulnerability on transitive dependencies from temporal-sdk:1.32.1
     implementation("net.minidev:json-smart:2.5.2")
     implementation("com.fasterxml.jackson.core:jackson-core:2.15.0")
     implementation("io.grpc:grpc-netty-shaded:1.75.0")
-    testImplementation("io.temporal:temporal-testing:1.31.0")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(platform("io.grpc:grpc-bom:1.75.0"))
     testImplementation("net.minidev:json-smart:2.5.2")
+
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useJUnit()
+}
+
+tasks.register<JavaExec>("runWorker") {
+    group = "temporal"
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("helloworldapp.workers.HelloWorldWorker")
+}
+
+tasks.register<JavaExec>("runWorkflow") {
+    group = "temporal"
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("helloworldapp.workflows.InitiateHelloWorldWorkflow")
 }
